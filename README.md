59b961ab296131faaa4247e8b531a915a1af0cc1
ptd3v <tovey@live.co.uk>
2023-03-19 22:30:09 +0000

    Update 0.1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Python URL Scraper
- Python URL Scraper
+To use this script, simply run it in a Python environment and enter the URL of the web page you want to scrape when prompted.
+The script will then scrape all the links on the page and return a Pandas data frame with the URL and title of each link.
--- webscraper.py
+++ webscraper.py
@@ -1,5 +1,28 @@
-from selenium import webdriver
+#Import Requests module
+import requests
+#Import BeautifulSoup 4
+from bs4 import BeautifulSoup
+#Import Pandas module
+import pandas as pd
 
-url = "https://www.ptd3v.dev/"
-browser = webdriver.Chrome()
-browser.get(url)
+#Takes a URL, stores the input as a variable called response
+url = input("Enter the URL of the URL you would like to scrape: ")
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+links = []
+for link in soup.find_all('a'):
+    href = link.get('href')
+    if href.startswith('https'):
+        links.append(href)
+
+data = []
+for link in links:
+    response = requests.get(link)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    title = soup.title.string if soup.title else None
+    data.append({'URL': link, 'Title': title})
+
+df = pd.DataFrame(data)
+print(df)
