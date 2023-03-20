746efc0a36de6d5e3d02683c7b090ee12c96f518
ptd3v <tovey@live.co.uk>
2023-03-20 16:00:03 +0000

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -3,22 +3,21 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-#Takes a URL, stores the input as a variable called response.
+#Takes a URL, stores the input as a variable called response. Loads the bs4 html parser.
 url = input("Please enter the address of the URL you would like to scrape: ")
 response = requests.get(url)
-
-#BeautifulSoup html parser
 soup = BeautifulSoup(response.text, 'html.parser')
 
 # create an empty list to store the links and titles
 url_and_titles = []
 
-# find all the links on the page and extract their URLs and titles. Appends to the empty list.
+# Finds all links on the page and extracts their URLs/ titles. Appends to the empty list.
 for link in soup.find_all("a"):
     href = link.get("href")
     title = link.get("title") or link.text.strip()
     url_and_titles.append({"URL": href, "title": title})
 
+#Saves the results as a dataframe, prints the results in the console and exports it as a csv.
 result = pd.DataFrame(url_and_titles)
 result.to_csv("urls_and_titles.csv", index=False)
 print(result)
